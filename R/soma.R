3b1868ebcb079c80626a170d7cc45636c00f818a
Beatriz Milz <beatriz.milz@hotmail.com>
2021-08-28 10:17:12 -0300

    arquivos do meu pacote

---
+++ R/soma.R
@@ -0,0 +1,12 @@
+#' funcao para somar dois numeros
+#'
+#' @param a um numero para somar
+#' @param b um numero para somar
+#'
+#' @return numero
+#' @export
+#'
+#' @examples
+fun <- function(a, b) {
+  a + b
+}
